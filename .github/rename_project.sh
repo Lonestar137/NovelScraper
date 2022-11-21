6b8c0c87fce4cb3d96982156c90f4c86169cbee3
Lonestar137 <Lonestar137@users.noreply.github.com>
2022-11-21 02:19:01 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Lonestar137"
+original_name="novelscraper"
+original_urlname="NovelScraper"
+original_description="Awesome novelscraper created by Lonestar137"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv novelscraper $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
